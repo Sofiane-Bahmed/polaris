7c668bc05a2b254205efef7ce587b83312ae675d
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2026-01-08 10:33:08 +0100

    04: background jobs

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function DemoPage() {
+    const [loading, setLoading] = useState(false);
+    const [loading2, setLoading2] = useState(false);
+
+    const handleBlocking = async () => {
+        setLoading(true);
+        await fetch('/api/demo/blocking', {
+            method: 'POST',
+        })
+        setLoading(false);
+    }
+    const handleBackground = async () => {
+        setLoading2(true);
+        await fetch('/api/demo/background', {
+            method: 'POST',
+        })
+        setLoading2(false);
+    }
+
+    return (
+        <div className="p-8 space-x-4 " >
+            <Button
+                disabled={loading}
+                onClick={handleBlocking}
+            >
+                {loading ? 'Generating...' : 'Generate Blocking Text'}
+            </Button>
+            <Button
+                disabled={loading2}
+                onClick={handleBackground}
+            >
+                {loading2 ? 'Generating...' : 'Generate Background Text'}
+            </Button>
+        </div>
+    )
+}
